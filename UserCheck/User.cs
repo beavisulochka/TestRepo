e025aef8493260960b7cda08eff9c770faec6f48
beavisulochka <102587784+beavisulochka@users.noreply.github.com>
2022-12-19 21:15:23 +0100

    Update User.cs

--- UserCheck/User.cs
+++ UserCheck/User.cs
@@ -5,49 +5,47 @@ namespace UserCheck
 {
 	public class User
 	{
-		public string Name { get; set; }
-        public string Password { get; set; }
+	    	public string Name { get; set; }
+        	public string Password { get; set; }
 
-        public User()
+        	public User()
 		{
-            AccountCreate();
-        }
+            		AccountCreate();
+        	}
 
 		public virtual void AccountCreate()
 		{
 			Console.WriteLine("Hello, write your user name please");
 			Name = Console.ReadLine();
-            Console.WriteLine("Yep, name was saved.");
+            		Console.WriteLine("Yep, name was saved.");
 
-            Console.WriteLine("Now time for your password(must be eight symbols min).");
-            string code = Console.ReadLine();
+            		Console.WriteLine("Now time for your password(must be eight symbols min).");
+            		string code = Console.ReadLine();
 
-            while (code.Length < 8)
-            {
-                Console.WriteLine("Password must be eight symbols min, write your password again please");
+            		while (code.Length < 8)
+            		{
+                		Console.WriteLine("Password must be eight symbols min, write your password again please");
 
+                		if (code.Length >= 8)
+                		{
+                   			Console.WriteLine("Great, confrim password please.");
+                    			string confrim = Console.ReadLine();
 
-                if (code.Length >= 8)
-                {
-                    Console.WriteLine("Great, confrim password please.");
-                    string confrim = Console.ReadLine();
+                    			while (confrim != code)
+                    			{
+                        			Console.WriteLine("Code not right, try again:(");
+                        			confrim = Console.ReadLine();
 
-                    while (confrim != code)
-                    {
-                        Console.WriteLine("Code not right, try again:(");
-                        confrim = Console.ReadLine();
-
-                        if (confrim == code)
-                        {
-                            confrim = Password;
-                            Console.WriteLine("Great, your password saved!.");
-                            break;
-                        }
-                    }
-
-                }
-            }
-        }
+                        			if (confrim == code)
+                        			{
+                            				confrim = Password;
+                            				Console.WriteLine("Great, your password saved!.");
+                            				break;
+                        			}
+                    			}				
+                		}
+            		}
+		}
 	}
 }
 
