5a5f57ef9cc2cff1ca8590e54a9492a098b3365a
beavisulochka <102587784+beavisulochka@users.noreply.github.com>
2022-12-19 21:17:32 +0100

    Update UserPro.cs

--- UserCheck/UserPro.cs
+++ UserCheck/UserPro.cs
@@ -11,13 +11,13 @@ namespace UserCheck
 			AccountCreate();
 		}
 
-        public override void AccountCreate()
-        {
-            base.AccountCreate();
+        	public override void AccountCreate()
+        	{
+            		base.AccountCreate();
 			EmailCreate();
 			PostCodeCreate();
 
-        }
+        	}
 
 		public void EmailCreate()
 		{
@@ -28,34 +28,34 @@ namespace UserCheck
  			while (!answer.Contains(dog) && !answer.Contains(dot)) 
 			{
 				Console.WriteLine("Usualy email have dots and '@' symbols, try to write email again");
-                answer = Console.ReadLine();
+                		answer = Console.ReadLine();
 
-                if (answer.Contains(dog) && answer.Contains(dot))
+                		if (answer.Contains(dog) && answer.Contains(dot))
 				{
-                    Email = answer;
-                    Console.WriteLine("Your email was saved.");
+                    			Email = answer;
+                    			Console.WriteLine("Your email was saved.");
 					break;
-                }
-            }
-        }
+                		}
+            		}
+       	 	}
 
 		public void PostCodeCreate()
 		{
-            Console.WriteLine("Wite your postcode for shipping information");
-            int answer = int.Parse(Console.ReadLine());
+            		Console.WriteLine("Wite your postcode for shipping information");
+            		int answer = int.Parse(Console.ReadLine());
 
-            while (answer.ToString().Length > 5 && answer.ToString().Length < 5)
+            		while (answer.ToString().Length > 5 && answer.ToString().Length < 5)
 			{
-                Console.WriteLine("Try again, postcode have only 5 numbers.");
-                answer = int.Parse(Console.ReadLine());
+                		Console.WriteLine("Try again, postcode have only 5 numbers.");
+                		answer = int.Parse(Console.ReadLine());
 
 				if(answer.ToString().Length == 5)
 				{
 					PostCode = answer;
-                    Console.WriteLine("Great, your postcode saved.");
-                }
-            }
-        }
-    }
+                    			Console.WriteLine("Great, your postcode saved.");
+               	 		}
+            		}
+        	}
+    	}
 }
 
